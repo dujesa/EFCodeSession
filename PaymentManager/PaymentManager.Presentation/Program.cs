70a73997c0e4266687873c04676187750633faf9
dujesaric <duje.saric@littlecode.com>
2021-12-18 22:35:48 +0100

    Project architecture
    Architecture setup and added nuget package dependencies.

---
+++ PaymentManager/PaymentManager.Presentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PaymentManager.Presentation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
